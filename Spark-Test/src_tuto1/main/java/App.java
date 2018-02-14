9edda78f6e34e8fe356fbdd90bcf7b35cd3e5382
Greg Tordjman <greg@MacBook-Pro-de-Greg.local>
2018-02-14 01:23:39 +0100

    1er Tuto

---
+++ Spark-Test/src_tuto1/main/java/App.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "hello.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/favorite_photos", (request, response) -> {
+            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
